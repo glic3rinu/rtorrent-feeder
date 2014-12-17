9b8053869548482d67f0df05022e26225483e681
Marc Aymerich <glicerinu@gmail.com>
2014-12-17 16:38:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ logging.basicConfig(level=settings.LOG_LEVEL or logging.ERROR)
 
 feeders = [
     downloaders.KickAssDownloader(),
-    downloaders.TPBDownloader(),
-    downloaders.EZRSSDownloader(),
+#    downloaders.TPBDownloader(),
+#    downloaders.EZRSSDownloader(),
 ]
 
 
@@ -20,13 +20,12 @@ if settings.SUBTITLES_PATH:
 downloads = []
 for feeder in feeders:
     try:
-        downloads += feeder.download()
+        for download in feeder.download():
+            downloads.append(download)
     except IOError:
         pass
-
 
 if downloads:
     utils.save_state()
     if settings.EMAIL_USER:
         utils.send_email(downloads)
-
